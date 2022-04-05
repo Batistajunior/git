bf3917876be83bccece5405be2f682cc50d21614
Batistajunior <86987034+Batistajunior@users.noreply.github.com>
2022-04-05 17:11:26 -0300

    Criação do projeto

---
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJave.java
@@ -0,0 +1,12 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJave {
+
+	/*main é um método auto executavel em java*/
+	
+	public static void main(String[] args) {
+		
+		/*escreve o texto no console*/
+		System.out.println("Meu Primeiro Programa em Java");
+	}
+}
